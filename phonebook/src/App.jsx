4320f963f19c0642e4c1cf83a9f5549ed07210ce
SirTwirl <j.kruk123@o2.pl>
2026-09-08 14:14:50 +0200

    feat(phonebook): allow adding new names

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,24 +1,51 @@
 import { useState } from 'react'
 
+const Person = ({text}) => (
+  <p>{text}</p>
+)
+
+const Persons = ({persons}) => (
+  <div>
+    {persons.map(person =>
+    <Person key={person.name} text={person.name}/>
+  )}
+  </div>
+)
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'}, 
+    { name: 'Ada Lovelace'}
   ]) 
   const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    console.log(nameObject)
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onSubmit={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Persons persons={persons}/>
     </div>
   )
 }
